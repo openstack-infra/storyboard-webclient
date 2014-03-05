55d4887d161b4885261dcd0cb9df1f621b9d8692
Michael Krotscheck <krotscheck@gmail.com>
2014-03-05 12:48:03 -0800

    Create story modal allows multi-task creation.
    The story modal now allows the creation of multiple tasks in one
    use action. When only one task is active, and it has not been
    modified, the title is slaved to the title of the story, however
    as soon as the task title is modified, it becomes independently
    editable. In addition, projects are independently selectable
    per task.
    
    Change-Id: If1d8e06b2a79708d117e805788854e8da0e1933f

--- src/app/stories/controllers/story_modal_controller.js
+++ src/app/stories/controllers/story_modal_controller.js
@@ -22,27 +22,87 @@ angular.module('sb.story').controller('StoryModalController',
         'use strict';
 
         $scope.projects = Project.query({});
-        $scope.story = new Story();
-        $scope.task = new Task({
+        $scope.story = new Story({title: ''});
+        $scope.tasks = [new Task({
+            title: '',
             project_id: params.projectId || null
-        });
+        })];
 
+        var lastTitle = '', trackingStoryTitle = true;
+        $scope.$on('$destroy', $scope.$watch(
+            function () {
+                return $scope.story.title;
+            },
+            function (newTitle) {
+
+                // Exit if we've set a custom title.
+                if (!trackingStoryTitle) {
+                    return;
+                }
+
+                // Get the first task in the list.
+                var task = $scope.tasks[0];
+                if (trackingStoryTitle && task.title === lastTitle) {
+                    task.title = newTitle;
+                    lastTitle = newTitle;
+                } else {
+                    trackingStoryTitle = false;
+                }
+            }
+        ));
+
+        /**
+         * Saves the story, then saves all the tasks associated with that
+         * story.
+         */
         $scope.save = function () {
             $scope.story.$create(function (story) {
 
-                // Create the task here, since we'll have the project_id
-                // bound to it.
-                $scope.task.title = story.title;
-                $scope.task.story_id = story.id;
+                var resolvingTasks = $scope.tasks.length;
 
-                $scope.task.$create(function () {
-                    $modalInstance.dismiss('success');
+                // Now that we've created the task, save all the tasks.
+                $scope.tasks.forEach(function (task) {
+                    task.story_id = story.id;
+                    task.$create(function () {
+                        resolvingTasks--;
+                        if (resolvingTasks === 0) {
+                            $modalInstance.dismiss('success');
+                        }
+                    });
                 });
             });
         };
 
+        /**
+         * Close this modal without saving.
+         */
         $scope.close = function () {
             $modalInstance.dismiss('cancel');
+        };
+
+        /**
+         * Add another task.
+         */
+        $scope.addTask = function () {
+            // When adding a new modal, grab the project ID from the last
+            // item in the list.
+
+            var lastTask = $scope.tasks[$scope.tasks.length - 1];
+            $scope.tasks.push(new Task({
+                project_id: lastTask.project_id
+            }));
+        };
+
+        /**
+         * Remove a task from the task list, but only if we have
+         * more than one task.
+         */
+        $scope.removeTask = function (task) {
+            if ($scope.tasks.length < 2) {
+                return;
+            }
+            var idx = $scope.tasks.indexOf(task);
+            $scope.tasks.splice(idx, 1);
         };
     })
 ;
