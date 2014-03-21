14fd308716a6073e0931ff88bd3503bbf3f1dcb9
Michael Krotscheck <krotscheck@gmail.com>
2014-03-21 14:18:03 -0700

    Comments UI
    - Added comments resource
    - Added comment list controller to resolve author names.
    - Updated detail controller to allow comment control.
    - Major rework of the story UI to make room for a tasks and discussions.
    - New LESS file for discussions.
    - New directive: ngShiftEnter, to allow shift-enter comment submission.
    
    Change-Id: I8be85da39097683eb7660835c1cbc40524dd5b2c

---
+++ src/app/stories/controllers/story_detail_comment_controller.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Controller used for the comments section on the story detail page.
+ */
+angular.module('sb.story').controller('StoryDetailCommentController',
+    function ($scope, User) {
+        'use strict';
+
+        $scope.author = User.get({
+            id: $scope.comment.author_id
+        });
+    });
