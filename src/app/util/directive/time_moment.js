7ca867b09a0b9a3e63170e1b70a12048f4e29729
Yolanda Robla <yolanda.robla-mota@hp.com>
2014-05-30 12:27:10 +0200

    Display dates in timeago format
    Add angular-momentjs to dependencies
    
    Update all date fields to include am-time-ago tag, to properly display dates
    
    Configure that using UTC timezone according to API requirements
    Timeago will be shown for changes done in less than one week, displaying
    a tooltip with whole date on hover.
    Full UTC date will be displayed for older changes.
    
    Change-Id: I2f4c1facaa3a99808f89900dbeb6b6cacb31f8a6
    Closes-Bug: 118

---
+++ src/app/util/directive/time_moment.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * A story status label that automatically selects color and text based on
+ * the bound-in story.
+ */
+angular.module('sb.util').directive('timeMoment',
+    function (DateUtil) {
+        'use strict';
+
+        return {
+            restrict: 'A',
+            templateUrl: 'app/templates/util/time_moment.html',
+            scope: {
+                eventdate: '='
+            },
+            controller: function ($scope) {
+
+                /**
+                 * Helper method to update the needs_timeago propery
+                 */
+                function updateTimeAgo() {
+                    $scope.needsTimeAgo =
+                        DateUtil.needsTimeAgo($scope.eventdate);
+                }
+
+                var unwatch = $scope.$watch(updateTimeAgo);
+                $scope.$on('$destroy', unwatch);
+
+                updateTimeAgo();
+            }
+        };
+    }
+);
