345bc56c567e80688783dcffa07002e22bb3222f
Michael Krotscheck <krotscheck@gmail.com>
2014-07-07 14:55:46 -0700

    Admin Project Group UI
    User interface for project group administration. Nothing fancy -
    I had to replace the Projects icon with one that had a plural version
    (box, boxes). There's an outstanding question on whether or not
    project groups should be editable.
    
    Change-Id: Ie117bbdf35f385ba016cb5a818cf05cd12ad7361

---
+++ src/app/admin/controller/project_group_item_controller.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Controller for the project group member list.
+ */
+angular.module('sb.admin').controller('ProjectGroupItemController',
+    function ($scope, $log, ProjectGroupItem) {
+        'use strict';
+
+        $scope.projectGroupItems = [];
+        $scope.loadingProjectGroupItems = false;
+
+        if (!$scope.projectGroup) {
+            return;
+        }
+
+        var id = $scope.projectGroup.id;
+
+        $scope.loadingProjectGroupItems = true;
+        ProjectGroupItem.query({
+                projectGroupId: id
+            },
+            function (results) {
+                $scope.loadingProjectGroupItems = false;
+                $scope.projectGroupItems = results;
+            }, function (error) {
+                $log.error(error);
+                $scope.loadingProjectGroupItems = false;
+            });
+    });
