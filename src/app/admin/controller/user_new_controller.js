74638192ab737167fbb078ffbaf6bab062ea2608
Michael Krotscheck <krotscheck@gmail.com>
2014-07-24 12:16:07 -0700

    Added user administration interface
    Users may now be searched, added, and edited. Requires
    https://review.openstack.org/112174
    
    Change-Id: Iae22f71e32030d814cdef702e00ce138ab1b12dd

---
+++ src/app/admin/controller/user_new_controller.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * New User modal controller.
+ */
+angular.module('sb.admin').controller('UserNewController',
+    function ($log, $scope, $modalInstance, User) {
+        'use strict';
+
+        /**
+         * Flag for the UI to indicate that we're saving.
+         *
+         * @type {boolean}
+         */
+        $scope.isSaving = false;
+
+        /**
+         * The new user.
+         *
+         * @type {User}
+         */
+        $scope.user = new User();
+
+        /**
+         * Saves the user group
+         */
+        $scope.save = function () {
+            $scope.isSaving = true;
+
+            // Create a new user group
+            $scope.user.$create(function (user) {
+                $modalInstance.close(user);
+            }, function (error) {
+                $scope.isSaving = false;
+                $log.error(error);
+                $modalInstance.dismiss(error);
+            });
+        };
+
+        /**
+         * Close this modal without saving.
+         */
+        $scope.close = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
