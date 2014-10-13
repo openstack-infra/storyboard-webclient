c7e518a952a706b3312572055a7a5efb50113394
Michael Krotscheck <krotscheck@gmail.com>
2014-10-13 15:58:45 -0700

    Added project group list and detail view.
    Reused several components from the existing list search, as well
    as other parts from project views, to create a rudimentary list of
    project groups. Also created a detail page that shows member
    projects and stories, and lastly added a quicknav option into the
    header quicknav.
    
    Change-Id: Ie29dacb6c6fda47c0e59fa054da9c259a9a59797

---
+++ src/app/project_group/module.js
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * The StoryBoard project group submodule handles most activity involving
+ * searching for and reviewing project groups. Administration of project groups
+ * lives in the admin module.
+ */
+angular.module('sb.project_group',
+    ['ui.router', 'sb.services', 'sb.util', 'sb.auth'])
+    .config(function ($stateProvider, $urlRouterProvider, PermissionResolver) {
+        'use strict';
+
+        // Routing Defaults.
+        $urlRouterProvider.when('/project_group', '/project_group/list');
+
+        // Set our page routes.
+        $stateProvider
+            .state('project_group', {
+                abstract: true,
+                url: '/project_group',
+                template: '<div ui-view></div>',
+                resolve: {
+                    isSuperuser: PermissionResolver
+                        .resolvePermission('is_superuser', true)
+                }
+            })
+            .state('project_group.list', {
+                url: '/list',
+                templateUrl: 'app/project_group/template/list.html',
+                controller: 'ProjectGroupListController'
+            })
+            .state('project_group.detail', {
+                url: '/{id:[0-9]+}',
+                templateUrl: 'app/project_group/template/detail.html',
+                controller: 'ProjectGroupDetailController',
+                resolve: {
+                    projectGroup: function ($stateParams, ProjectGroup, $q) {
+                        var deferred = $q.defer();
+
+                        ProjectGroup.get({id: $stateParams.id},
+                            function (result) {
+                                deferred.resolve(result);
+                            }, function (error) {
+                                deferred.reject(error);
+                            });
+                        return deferred.promise;
+                    },
+                    projects: function($stateParams, Project, $q) {
+                        var deferred = $q.defer();
+
+                        Project.query({project_group_id: $stateParams.id},
+                            function (result) {
+                                deferred.resolve(result);
+                            }, function (error) {
+                                deferred.reject(error);
+                            });
+                        return deferred.promise;
+                    },
+                    stories: function($stateParams, Story, $q) {
+                        var deferred = $q.defer();
+
+                        Story.query({project_group_id: $stateParams.id},
+                            function (result) {
+                                deferred.resolve(result);
+                            }, function (error) {
+                                deferred.reject(error);
+                            });
+                        return deferred.promise;
+                    }
+                }
+            });
+    })
+;
