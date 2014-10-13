edc090a209dca9d3af8f4bf57412cc6ffde68193
Michael Krotscheck <krotscheck@gmail.com>
2014-10-13 15:58:45 -0700

    Added project group list and detail view.
    Reused several components from the existing list search, as well
    as other parts from project views, to create a rudimentary list of
    project groups. Also created a detail page that shows member
    projects and stories, and lastly added a quicknav option into the
    header quicknav.
    
    Change-Id: Ie29dacb6c6fda47c0e59fa054da9c259a9a59797

---
+++ src/app/project_group/controller/project_group_detail_controller.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+/**
+ * Project group detail controller, for general user use of project groups.
+ * From a feature standpoint this really just means viewing the group, member
+ * projects, and any stories that belong under this project group.
+ */
+angular.module('sb.project_group').controller('ProjectGroupDetailController',
+    function ($scope, projectGroup, projects, stories) {
+        'use strict';
+
+        /**
+         * The project group we're viewing right now.
+         *
+         * @type ProjectGroup
+         */
+        $scope.projectGroup = projectGroup;
+
+        /**
+         * The list of projects in this group
+         *
+         * @type [Project]
+         */
+        $scope.projects = projects;
+
+        /**
+         * The list of stories in this project group
+         *
+         * @type [Story]
+         */
+        $scope.stories = stories;
+    });
