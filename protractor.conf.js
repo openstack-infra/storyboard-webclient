c45909b6f921f80d1266484e0b778651d410cb40
Joshua Hesketh <josh@nitrotech.org>
2015-01-21 21:43:47 +1100

    Increase protractor timeout
    Change-Id: Id895cffa0a15d4828e4131bfbb26565b215bc742

--- protractor.conf.js
+++ protractor.conf.js
@@ -29,7 +29,7 @@ exports.config = {
 
     // The timeout for each script run on the browser. This should be longer
     // than the maximum time your application needs to stabilize between tasks.
-    allScriptsTimeout: 11000,
+    allScriptsTimeout: 110000,
 
     // ----- What tests to run -----
     //
