12b8802e48fd2afa498b2d5d6c4ad05b187f757c
Michael Krotscheck <krotscheck@gmail.com>
2015-01-21 19:04:13 -0800

    An experimental improvement on the dashboard event list.
    This improvement on the dashboard attempts to improve context
    for individual events by grouping them by story and permitting
    group delete actions. It is not yet fully functional, open for commentary.
    
    Change-Id: I09bafce2813a67dcae7974e4b23015e48f11c333

--- src/app/dashboard/controller/dashboard_controller.js
+++ src/app/dashboard/controller/dashboard_controller.js
@@ -33,6 +33,8 @@ angular.module('sb.dashboard').controller('DashboardController',
             SubscriptionEvent.browse({
                 subscriber_id: currentUser.id
             }, function (results) {
+                var story_event_map = {};
+                var events = [];
 
                 // First go through the results and decode the event info.
                 results.forEach(function (row) {
@@ -43,11 +45,29 @@ angular.module('sb.dashboard').controller('DashboardController',
                     }
                 });
 
-                $scope.subscriptionEvents = results;
+                // Pull out individual story ID's.
+                results.forEach(function (row) {
+                    // Sort by story_id found in the event info.
+                    if (row.event_info.hasOwnProperty('story_id')) {
+                        var story_id = row.event_info.story_id;
+                        if (!story_event_map.hasOwnProperty(story_id)) {
+                            story_event_map[story_id] = {
+                                story_id: story_id,
+                                story_title: row.event_info.story_title,
+                                event_type: 'story_events',
+                                events: []
+                            };
+                            events.push(story_event_map[story_id]);
+                        }
+                        story_event_map[story_id].events.push(row);
+                    } else {
+                        events.push(row);
+                    }
+                });
+
+                $scope.subscriptionEvents = events;
             });
         }
-
-        loadEvents();
 
         $scope.removeEvent = function (event) {
             var deferred = $q.defer();
