ae55e18af8c8b4c500e6f187ba86d44e2a44d875
Michael Krotscheck <krotscheck@gmail.com>
2015-02-13 17:20:00 -0800

    Read user preferences from backend
    Remove usage of localStorage to retrieve preferences, and add a
    Preference resource that uses the backend API. Preferences are now
    bootstrapped in the root state so that our cache is fresh. Also a
    couple of UI tweaks to the preferences page.
    
    Change-Id: Icf3072e24fef86848801a5c72f5833605b1afcd7

---
+++ src/app/services/provider/preference.js
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2015 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+/**
+ * Preference service, a convenience-cashing API in front of our UserPreference
+ * service, with resolving/refresh functionality.
+ */
+angular.module('sb.services').provider('Preference',
+    function () {
+        'use strict';
+
+        /**
+         * Singleton preference provider.
+         */
+        var preferenceInstance = null;
+
+        /**
+         * Registered default preferences.
+         */
+        var defaults = {};
+
+        /**
+         * Each module can manually declare its own preferences that it would
+         * like to keep track of, as well as set a default. During the config()
+         * phase, inject the Preference Provider and call 'addPreference()' to
+         * do so. An example is available at the bottom of this file.
+         */
+        this.addPreference = function (name, defaultValue) {
+            defaults[name] = defaultValue;
+        };
+
+        /**
+         * The actual preference implementation.
+         */
+        function Preference($q, $log, Session, AccessToken, UserPreference,
+                            SessionState) {
+
+            // Scope assignment.
+            var self = this;
+
+            /**
+             * The currently loaded preferences.
+             */
+            this.preferences = {};
+
+            /**
+             * This function resolves the user preferences. If a valid session
+             * is resolvable, it will load the preferences for the user.
+             * Otherwise it will resolve the default preferences.
+             *
+             * @returns {deferred.promise|*}
+             */
+            this._resolveUserPreferences = function () {
+                var deferred = $q.defer();
+
+                // First resolve the session.
+                var sessionPromise = Session.resolveSessionState();
+                sessionPromise.then(
+                    function (state) {
+                        if (state === SessionState.LOGGED_IN) {
+                            UserPreference.get({id: AccessToken.getIdToken()},
+                                function (prefs) {
+                                    deferred.resolve(prefs);
+                                }, function () {
+                                    deferred.resolve(defaults);
+                                });
+                        } else {
+                            deferred.resolve(defaults);
+                        }
+                    },
+                    function () {
+                        deferred.resolve(defaults);
+                    }
+                );
+
+                return deferred.promise;
+            };
+
+            /**
+             * Create a composite result of preference defaults and server
+             * provided defaults.
+             */
+            this.getAll = function () {
+                var result = {};
+                for (var def_key in defaults) {
+                    result[def_key] = this.get(def_key);
+                }
+
+                for (var key in this.preferences) {
+                    result[key] = this.preferences[key];
+                }
+
+                return result;
+            };
+
+            /**
+             * Save all the preferences in the passed hash.
+             */
+            this.saveAll = function (preferences) {
+                // Update the preferences.
+                for (var key in defaults) {
+                    if (preferences.hasOwnProperty(key)) {
+                        this.preferences[key] = preferences[key];
+                    }
+                }
+                return this._save();
+            };
+
+            /**
+             * Returns the value for a given preference.
+             */
+            this.get = function (key) {
+
+                // Is this a valid preference?
+                if (!defaults.hasOwnProperty(key)) {
+                    $log.warn('Attempt to get unregistered preference: ' +
+                        key);
+                    return null;
+                }
+
+                // If the value is unset, and we have a default,
+                // set that.
+                if (!this.preferences.hasOwnProperty(key)) {
+                    $log.warn('Setting default preference: ',
+                        key, defaults[key]);
+                    this.set(key, defaults[key]);
+                }
+
+                return this.preferences[key];
+            };
+
+            /**
+             * Save a preference and return the saving promise.
+             */
+            this.set = function (key, value) {
+                // Is this a valid preference?
+                if (!defaults.hasOwnProperty(key)) {
+                    $log.warn('Attempt to set unregistered preference: ' +
+                        key);
+                    return null;
+                }
+
+                // Store the preference.
+                this.preferences[key] = value;
+
+                return this._save();
+            };
+
+            /**
+             * Resolve the preferences.
+             */
+            this.refresh = function () {
+                var deferred = $q.defer();
+
+                // This should never fail, see implementation above.
+                this._resolveUserPreferences().then(
+                    function (newPrefs) {
+                        self.preferences = angular.copy(newPrefs);
+                        deferred.resolve(newPrefs);
+                    }
+                );
+
+                return deferred.promise;
+            };
+
+            /**
+             * Private save method.
+             */
+            this._save = function() {
+                var deferred = $q.defer();
+                this.preferences.$save({id: AccessToken.getIdToken()},
+                    function () {
+                        deferred.resolve();
+                    }, function () {
+                        deferred.resolve();
+                    });
+
+                return deferred.promise;
+            };
+        }
+
+        /**
+         * Factory getter - returns a configured instance of preference
+         * provider, as needed.
+         */
+        this.$get =
+            function ($injector) {
+                if (!preferenceInstance) {
+                    preferenceInstance = $injector.instantiate(Preference);
+                }
+                return preferenceInstance;
+            };
+    })
+    .config(function (PreferenceProvider) {
+        'use strict';
+
+        // WARNING: In all modules OTHER than the services module, this config
+        // block can appear anywhere as long as this module is listed as a
+        // dependency. In the services module, the config() block must appear
+        // AFTER the provider block. For more information,
+        // @see https://github.com/angular/angular.js/issues/6723
+
+        // Let our preference provider know about page_size.
+        PreferenceProvider.addPreference('page_size', 10);
+
+    });
