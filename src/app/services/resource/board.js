930813112ba51db69b35f04176658df58bc047d2
Adam Coldrick <adam.coldrick@codethink.co.uk>
2015-10-06 15:19:52 +0000

    Add resource wrappers for boards and worklists
    These wrappers provide a nice way to interact with the boards
    and worklists API.
    
    Change-Id: I3a171450e0fbb1fc0657d44b681697cfd6ec8ed5

---
+++ src/app/services/resource/board.js
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2015 Codethink Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * The angular resource abstraction that allows us to access projects groups.
+ *
+ * @see ResourceFactory
+ * @author Adam Coldrick
+ */
+angular.module('sb.services').factory('Board',
+    function (ResourceFactory, Worklist, $resource, storyboardApiBase) {
+        'use strict';
+
+        var resource = ResourceFactory.build(
+            '/boards/:id',
+            '/boards/search',
+            {id: '@id'}
+        );
+
+        resource.Permissions = $resource(
+            storyboardApiBase + '/boards/:id/permissions',
+            {id: '@id'}
+        );
+
+        ResourceFactory.applySearch(
+            'Board',
+            resource,
+            'title',
+            {
+                Text: 'title',
+                Project: 'project_id',
+                User: 'creator_id'
+            }
+        );
+
+        resource.getLane = function(board, id) {
+            for (var n = 0; n < board.lanes.length; n++) {
+                if (board.lanes[n].list_id === id) {
+                    return board.lanes[n];
+                }
+            }
+        };
+
+        resource.loadContents = function(board, loadContents,
+                                         resolveContents) {
+            // The empty string is used to pass a false value to the endpoint.
+            // See: https://bugs.launchpad.net/wsme/+bug/1493982
+            Worklist.browse({
+                board_id: board.id,
+                hide_lanes: ''
+            }).$promise.then(function(worklists) {
+                board.worklists = worklists.sort(function(a, b) {
+                    return resource.getLane(board, a.id).position -
+                           resource.getLane(board, b.id).position;
+                });
+            }).then(function() {
+                if (loadContents) {
+                    for (var i = 0; i < board.worklists.length; i++) {
+                        var worklist = board.worklists[i];
+                        Worklist.loadContents(worklist, resolveContents);
+                    }
+                }
+            });
+        };
+
+        return resource;
+    });
---
+++ src/app/services/resource/worklist.js
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2015 Codethink Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * The angular resource abstraction that allows us to access projects groups.
+ *
+ * @see ResourceFactory
+ * @author Adam Coldrick
+ */
+angular.module('sb.services').factory('Worklist',
+    function (ResourceFactory, $resource, storyboardApiBase, Task, Story) {
+        'use strict';
+
+        var resource = ResourceFactory.build(
+            '/worklists/:id',
+            '/worklists/search',
+            {id: '@id'}
+        );
+
+        var items_signature = {
+            'create': {
+                method: 'POST'
+            },
+            'get': {
+                method: 'GET',
+                cache: false,
+                isArray: true
+            },
+            'update': {
+                method: 'PUT'
+            },
+            'delete': {
+                method: 'DELETE',
+                transformRequest: function() {
+                    return '';
+                }
+            }
+        };
+
+        resource.ItemsController = $resource(
+            storyboardApiBase + '/worklists/:id/items/:item_id',
+            {id: '@id', item_id: '@item_id'},
+            items_signature);
+
+        ResourceFactory.applySearch(
+            'Worklist',
+            resource,
+            'title',
+            {
+                Text: 'title',
+                Project: 'project_id',
+                User: 'creator_id'
+            }
+        );
+
+        function addItem(list, listitem) {
+            return function(item) {
+                item.list_item_id = listitem.id;
+                item.type = listitem.item_type;
+                item.position = listitem.list_position;
+                list.push(item);
+                list.sort(function(a, b) {
+                    return a.position - b.position;
+                });
+            };
+        }
+
+        resource.resolveContents = function(worklist, items) {
+            for (var i = 0; i < items.length; i++) {
+                var item = items[i];
+                if (item.item_type === 'task') {
+                    Task.get({
+                        id: item.item_id
+                    }).$promise.then(
+                        addItem(worklist.items, item));
+                } else if (item.item_type === 'story') {
+                    Story.get({
+                        id: item.item_id
+                    }).$promise.then(
+                        addItem(worklist.items, item));
+                }
+            }
+        };
+
+        resource.loadContents = function (worklist, resolveContents) {
+            resource.ItemsController.get({
+                id: worklist.id
+            }).$promise.then(function(items) {
+                if (resolveContents) {
+                    worklist.items = [];
+                    resource.resolveContents(worklist, items);
+                } else {
+                    worklist.items = items;
+                }
+            });
+        };
+
+
+        return resource;
+    });
