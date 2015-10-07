0864a6e8d243fe56b7eaf9c34ed00cebd26cf917
Adam Coldrick <adam.coldrick@codethink.co.uk>
2015-10-07 10:00:26 +0000

    Add a page to view a board
    Add a board detail page, and a modal template and controller for
    archiving the worklist.
    
    Change-Id: If537451d2838938d8df9728dc265a4d7f8fd0509

--- src/app/storyboard/module.js
+++ src/app/storyboard/module.js
@@ -26,7 +26,7 @@ angular.module('storyboard',
     [ 'sb.services', 'sb.templates', 'sb.dashboard', 'sb.pages', 'sb.projects',
         'sb.auth', 'sb.story', 'sb.profile', 'sb.notification', 'sb.search',
         'sb.admin', 'sb.subscription', 'sb.project_group', 'sb.worklist',
-        'ui.router', 'ui.bootstrap', 'monospaced.elastic',
+        'sb.board', 'ui.router', 'ui.bootstrap', 'monospaced.elastic',
         'angularMoment', 'angular-data.DSCacheFactory', 'viewhead',
         'ngSanitize', 'as.sortable'])
     .constant('angularMomentConfig', {
