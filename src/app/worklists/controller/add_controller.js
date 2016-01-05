b4ae3f3bae040c24cb1dcd291403818c906aabb5
Adam Coldrick <adam.coldrick@codethink.co.uk>
2016-01-05 13:09:00 +0000

    Improve usability of the "New Worklist" modal
    This patch makes the "New Worklist" modal nicer to use. The
    improvements are:
    
    * An indication that the title is the only required field.
    * An indication that something is happening when the "Save Changes"
      button is clicked.
    * The "Save Changes" button is greyed out until the worklist is valid.
    * The form is "locked" when the worklist is being saved.
    
    Change-Id: I196f18f070492c1c2a062cd07b967c1cbf13b4ff

--- src/app/worklists/controller/add_controller.js
+++ src/app/worklists/controller/add_controller.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Codethink Limited
+ * Copyright (c) 2015-2016 Codethink Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License. You may obtain
@@ -25,8 +25,10 @@ angular.module('sb.worklist').controller('AddWorklistController',
          * Saves the worklist.
          */
         $scope.save = function () {
+            $scope.isSaving = true;
             $scope.worklist.$create(
                 function (result) {
+                    $scope.isSaving = false;
                     $modalInstance.dismiss('success');
                     $state.go('sb.worklist.detail', {worklistID: result.id});
                 }
@@ -40,5 +42,6 @@ angular.module('sb.worklist').controller('AddWorklistController',
             $modalInstance.dismiss('cancel');
         };
 
+        $scope.isSaving = false;
         $scope.worklist = new Worklist({title: ''});
     });
