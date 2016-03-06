d0f003b82a2cfa457005cf3386fd36e4bd2de0e6
Adam Coldrick <adam@sotk.co.uk>
2016-03-06 19:47:24 +0000

    Add the ability to permalink to a comment
    This commit adds a button to comments which directs you to a link that
    will automatically scroll the page to the comment.
    
    The link can be manually constructed by appending `#comment-$ID` to the
    story's URL. For example, to link to a comment with ID 45, one would
    append `#comment-45`.
    
    Change-Id: Iacf43c81457b018a30a2f1a410cfa50bea972fef

--- src/app/stories/controller/story_detail_controller.js
+++ src/app/stories/controller/story_detail_controller.js
@@ -21,10 +21,15 @@ angular.module('sb.story').controller('StoryDetailController',
     function ($log, $rootScope, $scope, $state, $stateParams, $modal, Session,
               Preference, TimelineEvent, Comment, TimelineEventTypes, story,
               Story, creator, tasks, Task, DSCacheFactory, User, $q,
-              storyboardApiBase, SessionModalService, moment, $document) {
+              storyboardApiBase, SessionModalService, moment, $document,
+              $anchorScroll, $timeout, $location) {
         'use strict';
 
         var pageSize = Preference.get('story_detail_page_size');
+        var firstLoad = true;
+        // Set the yOffset to 50 because the fixed bootstrap navbar
+        // is 50px high.
+        $anchorScroll.yOffset = 50;
 
         /**
          * The story, resolved in the state.
@@ -82,6 +87,14 @@ angular.module('sb.story').controller('StoryDetailController',
                     });
                     $scope.events = eventResults;
                     $scope.isSearching = false;
+                    if (firstLoad) {
+                        firstLoad = false;
+                        // Wrap this in a timeout to make sure we don't
+                        // try to scroll before the timeline is rendered.
+                        $timeout(function() {
+                            $anchorScroll();
+                        }, 0);
+                    }
                 },
                 function () {
                     $scope.isSearching = false;
@@ -280,6 +293,16 @@ angular.module('sb.story').controller('StoryDetailController',
                 $scope.newComment.content = quoted;
             }
             $document[0].getElementById('comment').focus();
+        };
+
+        /**
+         * Determine if a comment is currently being permalinked
+         */
+        $scope.isLinked = function(event) {
+            if ($location.hash() === 'comment-' + event.comment.id) {
+                return true;
+            }
+            return false;
         };
 
         /**
