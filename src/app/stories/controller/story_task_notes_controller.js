8783ccc9c1cb1f6386bf7912811f184c654e78e4
Zara <zara.zaimeche@codethink.co.uk>
2016-03-08 22:28:35 +0000

    WIP task notes
    WIP patch to add notes to tasks. WIP because the code is very messy
    and we should update fontawesome first.
    
    Change-Id: Ib81fc44e84424a5d3a281b7230217d8a8c017c1f

---
+++ src/app/stories/controller/story_task_notes_controller.js
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License'); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Story detail &  manipulation controller.
+ */
+angular.module('sb.story').controller('StoryTaskNotesController',
+    function ($log, $scope, $state, task, $modalInstance) {
+        'use strict';
+
+        $scope.task = task;
+
+                /**
+         * Generic service error handler. Assigns errors to the view's scope,
+         * and unsets our flags.
+         */
+        function handleServiceError(error) {
+            // We've encountered an error.
+            $scope.error = error;
+            $scope.isLoading = false;
+            $scope.isUpdating = false;
+        }
+
+        /**
+         * Resets our loading flags.
+         */
+        function handleServiceSuccess() {
+            $scope.isLoading = false;
+            $scope.isUpdating = false;
+        }
+
+
+        /**
+         * Resets any changes and toggles the form back.
+         */
+        $scope.cancel = function () {
+            $modalInstance.close('cancel');
+        };
+
+
+        /**
+         * Scope method, invoke this when you want to update the task.
+         */
+        $scope.update = function () {
+            // Set our progress flags and clear previous error conditions.
+            $scope.isUpdating = true;
+            $scope.error = {};
+
+            // Invoke the save method and wait for results.
+            $scope.task.$update(
+                function () {
+                    $modalInstance.close('save');
+                    handleServiceSuccess();
+                },
+                handleServiceError
+            );
+        };
+
+        // Set our progress flags and clear previous error conditions.
+
+        $scope.close = function () {
+            $modalInstance.close('closed');
+        };
+    });
