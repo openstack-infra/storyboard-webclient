fcc709900b759e382784e6bb3fe337b5bf72a2ef
Zara <zara.zaimeche@codethink.co.uk>
2016-03-11 14:52:03 +0000

    Edit task notes from board
    This adds the option to display and edit task notes in the card modal.
    
    Change-Id: Ia4360369178a34443afaadcfdb41604c20695ed3

--- src/app/boards/controller/card_detail_controller.js
+++ src/app/boards/controller/card_detail_controller.js
@@ -189,13 +189,19 @@ angular.module('sb.board').controller('CardDetailController',
             $scope.editingAssignee = !$scope.editingAssignee;
         };
 
+        /**
+        * and notes
+        */ 
+            
         $scope.updateTask = function(task) {
             var params = {
                 id: task.id,
-                assignee_id: task.assignee_id
+                assignee_id: task.assignee_id,
+                link: task.link
             };
             Task.update(params, function() {
                 $scope.editingAssignee = false;
+                $scope.editingNotes = false;
             });
         };
 
@@ -248,9 +254,11 @@ angular.module('sb.board').controller('CardDetailController',
         $scope.board = board;
         $scope.permissions = permissions;
         $scope.showDescription = true;
+        $scope.showTaskNotes = true;
         $scope.assigningDueDate = false;
         $scope.editingDueDate = false;
         $scope.editingDescription = false;
         $scope.editingAssignee = false;
+        $scope.showTaskForm = false;
     }
 );
