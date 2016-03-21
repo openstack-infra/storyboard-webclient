e8332f4d412d030fc990ef89dcbba463bc9fb628
Adam Coldrick <adam.coldrick@codethink.co.uk>
2016-03-21 18:38:43 +0000

    Allow filters to be set on automatic worklists
    This commit adds a UI for creating and updating filters on worklists
    with the "automatic" flag set. This allows the creation of automatically
    populated worklists.
    
    Change-Id: I880f1aa2044efcd34eb8eb9c608f344432957e6a

--- src/app/worklists/controller/add_worklist_controller.js
+++ src/app/worklists/controller/add_worklist_controller.js
@@ -21,6 +21,16 @@ angular.module('sb.worklist').controller('AddWorklistController',
     function ($scope, $modalInstance, $state, params, Worklist) {
         'use strict';
 
+        var blankFilter = {
+            type: 'Story',
+            filter_criteria: [{
+                negative: false,
+                field: null,
+                value: null,
+                title: null
+            }]
+        };
+
         /**
          * Saves the worklist.
          */
@@ -42,6 +52,68 @@ angular.module('sb.worklist').controller('AddWorklistController',
             $modalInstance.dismiss('cancel');
         };
 
+        $scope.setType = function(type) {
+            $scope.newFilter.type = type;
+            $scope.resourceTypes = [type];
+            $scope.$broadcast('refresh-types');
+        };
+
+        $scope.setCriterion = function(criterion, tag) {
+            criterion.field = tag.type;
+            criterion.value = tag.value.toString();
+            criterion.title = tag.title;
+        };
+
+        $scope.addCriterion = function(filter) {
+            filter.filter_criteria.push({
+                negative: false,
+                field: null,
+                value: null,
+                title: null
+            });
+        };
+
+        $scope.removeTag = function(criterion) {
+            if ($scope.newFilter.filter_criteria.length > 1) {
+                var idx = $scope.newFilter.filter_criteria.indexOf(criterion);
+                $scope.newFilter.filter_criteria.splice(idx, 1);
+            } else {
+                criterion.field = null;
+                criterion.value = null;
+                criterion.title = null;
+            }
+        };
+
+        $scope.checkNewFilter = function() {
+            var valid = true;
+            angular.forEach(
+                $scope.newFilter.filter_criteria,
+                function(criterion) {
+                    if (criterion.field == null ||
+                        criterion.value == null ||
+                        criterion.title == null) {
+                        valid = false;
+                    }
+                }
+            );
+            return valid;
+        };
+
+        $scope.remove = function(filter) {
+            var idx = $scope.worklist.criteria.indexOf(filter);
+            $scope.worklist.criteria.splice(idx, 1);
+        };
+
+        $scope.saveNewFilter = function() {
+            var added = angular.copy($scope.newFilter);
+            $scope.worklist.filters.push(added);
+            $scope.showAddFilter = false;
+            $scope.newFilter = angular.copy(blankFilter);
+        };
+
         $scope.isSaving = false;
-        $scope.worklist = new Worklist({title: ''});
+        $scope.worklist = new Worklist({title: '', filters: []});
+        $scope.resourceTypes = ['Story'];
+        $scope.showAddFilter = true;
+        $scope.newFilter = angular.copy(blankFilter);
     });
