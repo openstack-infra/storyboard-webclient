4540658a007a7da0013801f44d695dd5e61b76cb
Adam Coldrick <adam@sotk.co.uk>
2016-06-17 11:32:51 +0100

    Make the Tags resource look for existing tags to filter by
    This commit causes the search bars to suggest existing tags based on
    what the user types, rather than just suggesting they try to filter
    by tags matching exactly what they've typed.
    
    Change-Id: Ibf9809f270616f495efd255ba253d8d741ba423a
    Depends-on: If13b29d050abd66428c6787121d6b61b357bc9fb

--- src/app/services/resource/tags.js
+++ src/app/services/resource/tags.js
@@ -18,22 +18,26 @@
 /**
  * This criteria resolver may be injected by individual resources that accept a
  * Tags search parameter.
+ *
+ * @see ResourceFactory
  */
 angular.module('sb.services').factory('Tags',
-    function (Criteria, $q) {
+    function (ResourceFactory) {
         'use strict';
 
-        /**
-         * Return a Tags search parameter constructed from the passed search
-         * string.
-         */
+        var resource = ResourceFactory.build(
+            '/tags/:id',
+            '/tags/search',
+            {id: '@id'}
+        );
 
-        return {
-            criteriaResolver: function (searchString) {
-                var deferred = $q.defer();
-                deferred.resolve([Criteria.create('Tags', searchString)]);
+        ResourceFactory.applySearch(
+            'Tags',
+            resource,
+            'name',
+            {},
+            true // Use the name field for browse criteria, instead of the ID
+        );
 
-                return deferred.promise;
-            }
-        };
+        return resource;
     });
